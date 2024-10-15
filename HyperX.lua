585958915f64d377625bea4a46f3346551752b19
NetHunTer <beamzbyhakz@proton.me>
2024-10-15 14:43:17 +0200

    Update HyperX.lua

--- HyperX.lua
+++ HyperX.lua
@@ -1,1 +1,8 @@
-hi
+local player = game.Players.LocalPlayer
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Credits", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Credits", 
+	PremiumOnly = false
